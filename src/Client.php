57ae003506c226e669f1ecd613c92e69761dd961
Christopher Pitt <cgpitt@gmail.com>
2020-03-26 21:17:24 +0200

    feat(add-stream-support): add the ability to stream responses for reduced memory usage

--- src/Client.php
+++ src/Client.php
@@ -79,7 +79,24 @@ class Client
     public function __construct(CredentialsInterface $credentials, $options = array(), HttpClient $client = null)
     {
         if (is_null($client)) {
-            $client = new \Http\Adapter\Guzzle6\Client();
+            $guzzle = null;
+
+            if (!empty($options['stream'])) {
+                $guzzleOptions = [
+                    'stream' => true,
+                ];
+
+                if (!empty($options['guzzleOptions'])) {
+                    $guzzleOptions = array_merge(
+                        $guzzleOptions,
+                        $options['guzzleOptions']
+                    );
+                }
+
+                $guzzle = new \GuzzleHttp\Client($guzzleOptions);
+            }
+
+            $client = new \Http\Adapter\Guzzle6\Client($guzzle);
         }
 
         $this->setHttpClient($client);
@@ -339,6 +356,14 @@ class Client
         );
 
         return $this->send($request);
+    }
+
+    /**
+     * @return resource A streamable response resource
+     */
+    public function stream(...$params)
+    {
+        return $this->get(...$params)->getBody()->detach();
     }
 
     /**
